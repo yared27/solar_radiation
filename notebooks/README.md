e1924d89fe92a1a67d6cebc6c7371ff03ff0965d
Yared <alemayehuyared@gmail.com>
2024-08-23 14:54:03 -0700

    Update notebooks and add new analysis

---
+++ notebooks/README.md
@@ -0,0 +1,2 @@
+a EDA of the benine dataset,we aim to fin a pattern to reach some conclusion based on the 
+analysis
---
+++ notebooks/__init__.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
